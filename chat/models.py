d969987f967d51097fbb56318edb0814af258e87
Nikita Zemtsov <zemtsovnikig@gmail.com>
2023-02-24 19:02:18 +0200

    Make chat app and models

---
+++ chat/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class ThreadModel(models.Model):
+    participants = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Thread:{self.pk}'
+
+
+class MessageModel(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField(blank=True)
+    thread = models.ForeignKey(ThreadModel, on_delete=models.CASCADE)
+    is_read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'Message: {self.pk} in thread: {self.thread}'
